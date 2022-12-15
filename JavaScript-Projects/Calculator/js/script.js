035cf03c510499f8f83b77cc1f98f7ffa74d4f0b
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-12-15 12:27:28 +0000

    Update

---
+++ JavaScript-Projects/Calculator/js/script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    let c = a + b;
+    return c;
+}
+
+function substract(a, b) {
+    let c = a - b;
+    return c;
+} 
+
+function multiply(a, b) {
+    let c = a*b;
+    return c;
+}
+
+function divide(a, b) {
+    let c = a/b;
+    return c;
+}
