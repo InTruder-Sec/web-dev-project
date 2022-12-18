2292f167c0020814bad581dec08d8f4858b7f6e0
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-12-18 17:06:16 +0000

    update

--- JavaScript-Projects/Calculator/js/script.js
+++ JavaScript-Projects/Calculator/js/script.js
@@ -1,3 +1,4 @@
+var value = 0;
 function add(a, b) {
     let c = a + b;
     return c;
@@ -16,4 +17,28 @@ function multiply(a, b) {
 function divide(a, b) {
     let c = a/b;
     return c;
+}
+
+function percentage(a, b) {
+    let c = (a/b)*100;
+    return c;
+}
+
+display = "";
+initial = "";
+function valueC(num) {
+    initial += num;
+    document.getElementById('display').placeholder = initial;
+    return 0;
+}
+
+function addOpp() {
+    if (value == 0) {
+        value = initial;
+        initial = "";
+    } else {
+        value = value + initial;
+        alert(value);
+        document.getElementById('display').placeholder = value;
+    }
 }
