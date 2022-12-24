85f676a9cfdb20d74cd8c2afeeda1133e4d34d68
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-12-24 07:30:19 +0000

    update

--- JavaScript-Projects/Clock/js/script.js
+++ JavaScript-Projects/Clock/js/script.js
@@ -1,9 +1,10 @@
+// Start of clock code
 
 function getTime() {
     const stat = new Date();
     let s = "AM"
     let hrs = stat.getHours();
-    if (hrs>12) {
+    if (hrs>=12) {
         hrs = hrs - 12;
         hrs = "0" + hrs;
         s = "PM" 
@@ -29,28 +30,76 @@ function getDate() {
     const stat = new Date();
     let q = stat.getDay();
     let day;
+    console.log(q);
     switch (q) {
-        case '1':
+        case 1:
             day = "Monday";
             break;
-        case '2':
+        case 2:
             day = "Tuesday";
             break;
-        case '3':
+        case 3:
             day = "Wednesday";
             break
-        case '4':
+        case 4:
             day = "Thrusday";
             break;
-        case '5':
+        case 5:
             day = "Friday";
             break;
-        case '6':
+        case 6:
             day = "Saturday";
             break;
-        case '7':
+        case 7:
             day = "Sunday";
             break;
     }
-    console.log(day);
+    let date;
+    date = stat.getDate();
+    let month = stat.getMonth();
+    console.log(month);
+    switch (month) {
+        case 1:
+            month = "January";
+            break;
+        case 2:
+            month = "February";
+            break;
+        case 3:
+            month = "March";
+            break;
+        case 4:
+            month = "April";
+            break;
+        case 5:
+            month = "May";
+            break;
+        case 6:
+            month = "June";
+            break;
+        case 7:
+            month = "July";
+            break;
+        case 8:
+            month = "August";
+            break;
+        case 9:
+            month = "September";
+            break; 
+        case 10:
+            month = "October";
+            break;
+        case 11:
+            month = "November";
+            break;
+        case 12:
+            month = "December";
+            break;
+    }
+    let newDate = day + " " + date + " " + month;
+    document.getElementById("date").innerHTML = newDate;
 }
+
+// End of clock 
+
+// Start of Stopwatch code
