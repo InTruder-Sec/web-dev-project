814db2eb2f02b6a071c17b596b246fdd10194277
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-01-01 19:26:42 +0000

    Update

--- JavaScript-Projects/Notes Keeper/js/script.js
+++ JavaScript-Projects/Notes Keeper/js/script.js
@@ -1,7 +1,19 @@
 function displayWindow(v) {
     if (v==1) {
-        document.getElementById("window").style.opacity = '1';
         document.getElementById("window").style.zIndex = "1";
-
+        setTimeout("changeOpacity()", 100);
+        document.getElementById("wrapper").style.opacity = "0.5";
+        document.getElementById("addNote").value = "0";
+        document.getElementById("addNote").style.rotate = "45deg";
+    } else {
+        document.getElementById("window").style.opacity = '0';
+        document.getElementById("window").style.zIndex = "-1";
+        document.getElementById("wrapper").style.opacity = "1";
+        document.getElementById("addNote").value = "1";
+        document.getElementById("addNote").style.rotate = "0deg";
     }
+}
+
+function changeOpacity() {
+    document.getElementById("window").style.opacity = '1';
 }
