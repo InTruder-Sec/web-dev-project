666258a1186daeb4fc3fc0f9ea584e6aea1b88c3
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-01-08 05:32:09 +0000

    New Project 8

---
+++ JavaScript-Projects/Virtual_keyboard/js/script.js
@@ -0,0 +1,11 @@
+
+let keyVal;
+function enterText(keyVal) {
+    let initVal = document.getElementById("textEnter").value;
+    if (keyVal == "del") {
+        initVal = initVal.substring(0,initVal.length-1);
+        document.getElementById("textEnter").value = initVal;
+    } else {
+        document.getElementById("textEnter").value += keyVal
+    }
+}
