aca87e3d291566c07b9d614e76cb67d4c6c4c985
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-01-10 12:50:37 +0000

    Update

--- JavaScript-Projects/Snake_Game/js/script.js
+++ JavaScript-Projects/Snake_Game/js/script.js
@@ -2,13 +2,16 @@
 let moveBtns = document.getElementById("moveBtns");
 let startBtn = document.getElementById("startBtn");
 let snakeImg = document.getElementById("snakeImg");
+let blockMainDiv = document.getElementById("blockMainDiv");
+let i;
 
 function startGame() {
     startBtn.style.opacity = 0;
     setTimeout("changeAxis()", 1000);
     moveBtns.style.opacity = 1;
     snakeImg.style.opacity = 0;
-    buildBlocks();
+    setTimeout("removeIMG()", 700);
+    setTimeout("buildBlocks()", 800);
 }
 
 function changeAxis() {
@@ -17,6 +20,27 @@ function changeAxis() {
     console.log("Done");
 }
 
-function buildBlocks() {
-    console.log("Work in progress");
+function removeIMG() {
+    snakeImg.style.display = "none";
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const buildBlocks = async () => {
+    for (i=1; i<=100; i++) {
+        await delay();
+        if (i%2==0) {
+            let content = '<div class="block" id="block' + i + '"value="' + i + '"></div>'
+            blockMainDiv.innerHTML += content;
+        } else {
+            let content = '<div class="block2" id="block' + i + '"value="' + i + '"></div>'
+            blockMainDiv.innerHTML += content;
+        }
+    }
+}
+
+function createDiv() {
+    i = parseInt(i);
+    
+    
 }
