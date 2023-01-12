9096862dd40a8c17ec67a430f73948e5ab4f5198
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-01-12 11:36:08 +0000

    Update

--- JavaScript-Projects/Snake_Game/js/script.js
+++ JavaScript-Projects/Snake_Game/js/script.js
@@ -5,6 +5,13 @@ let snakeImg = document.getElementById("snakeImg");
 let blockMainDiv = document.getElementById("blockMainDiv");
 let i;
 let t;
+let moveUp = false;
+let moveDown = false;
+let moveRight = true;
+let moveLeft = false;
+let head = 24;
+let tail = 21;
+
 
 function startGame() {
     startBtn.style.opacity = 0;
@@ -13,6 +20,7 @@ function startGame() {
     snakeImg.style.opacity = 0;
     setTimeout("removeIMG()", 700);
     setTimeout("buildBlocks()", 800);
+    setTimeout("displaySnake()", 7500);
 }
 
 function changeAxis() {
@@ -30,58 +38,48 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 const buildBlocks = async () => {
     t = 0;
     for (i=1; i<=100; i++) {
-        if (t=0) {
-            if (i%2==0) {
-                let content = '<div class="block" id="block' + i + ' "value="' + i + '"></div>';
+        await delay(50);
+        if (t===0) {
+            if (i%2===0) {
+                let content = '<div class="block" id="block' + i + '" value="' + i + '"></div>'
                 blockMainDiv.innerHTML += content;
             } else {
-                let content = '<div class="block2" id="block' + i + ' "value="' + i + '"></div>';
+                let content = '<div class="block2" id="block' + i + '"value="' + i + '"></div>'
                 blockMainDiv.innerHTML += content;
             }
-
-            if (t%10==0) {
+            if (i%10===0) {
                 t = 1;
             }
         } else {
-            if (i%2==0) {
-                let content = '<div class="block" id="block' + i + ' "value="' + i + '"></div>';
+            if (i%2===0) {
+                let content = '<div class="block2" id="block' + i + '" value="' + i + '"></div>'
                 blockMainDiv.innerHTML += content;
             } else {
-                let content = '<div class="block2" id="block' + i + ' "value="' + i + '"></div>';
+                let content = '<div class="block" id="block' + i + '"value="' + i + '"></div>'
                 blockMainDiv.innerHTML += content;
             }
-            if (t%10==0) {
+            if (i%10===0) {
                 t = 0;
             }
         }
     } 
-
-
-
-    // for (i=1; i<=100; i++) {
-    //     await delay(100);
-    //     if (t==0) {
-    //         if (i%2==0) {
-    //             let content = '<div class="block" id="block' + i + '"value="' + i + '"></div>'
-    //             blockMainDiv.innerHTML += content;
-    //         } else {
-    //             let content = '<div class="block2" id="block' + i + '"value="' + i + '"></div>'
-    //             blockMainDiv.innerHTML += content;
-    //         }
-    //         if (i%10==0) {
-    //             t = 1;
-    //         }
-    //     } else {
-    //         if (i%2==0) {
-    //             let content = '<div class="block2" id="block' + i + '"value="' + i + '"></div>'
-    //             blockMainDiv.innerHTML += content;
-    //         } else {
-    //             let content = '<div class="block" id="block' + i + '"value="' + i + '"></div>'
-    //             blockMainDiv.innerHTML += content;
-    //         }
-    //         if (i%10==0) {
-    //             t = 1;
-    //         }
-    //     }
-    // } 
 }
+
+
+const displaySnake = async () => {
+    document.getElementById("block21").style.backgroundColor = "#2DCDDF";
+    await delay(100);
+    document.getElementById("block22").style.backgroundColor = "#2DCDDF";
+    await delay(100);
+    document.getElementById("block23").style.backgroundColor = "#2DCDDF";
+    await delay(100);
+    document.getElementById("block24").style.backgroundColor = "#2DCDDF";
+}
+
+
+// const moveSnake = () => {
+//     if (moveUp == true) {
+
+//     }
+// }
+
