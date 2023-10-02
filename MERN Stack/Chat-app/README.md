1c99fb608dd4f2334fe7cabc95b212682ec28d3f
= <dhakatedeep909@gmail.com>
2023-10-02 12:08:33 +0530

    Feat: Updated app

--- MERN Stack/Chat-app/README.md
+++
@@ -1,9 +0,0 @@
-## Scribble chat app
-
-### Tech stack used
-
-1. ReactJS
-2. Moongose
-3. MongoDB
-4. Bcrypt
-5. Appwrite
--- MERN Stack/Chat-app/frontend/node_modules/react-sketch-canvas/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-sketch-canvas.cjs.production.min.js')
-} else {
-  module.exports = require('./react-sketch-canvas.cjs.development.js')
-}
