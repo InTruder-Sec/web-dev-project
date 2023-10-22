bec6383cc9a5f106d1acd5d143b8b368f9d37887
= <dhakatedeep909@gmail.com>
2023-10-22 15:39:30 +0530

    Fixing onetask app

--- MERN Stack/onetask/backend/controllers/TaskControllers/AddTasks.ts
+++ MERN Stack/onetask/backend/controllers/TaskControllers/AddTasks.ts
@@ -2,8 +2,6 @@ import { Request, Response } from "express";
 import userSession from "../UserControlers/userSession";
 import Users from "../../models/users";
 
-import mongoose from "mongoose";
-
 const AddTasks = async (req: Request, res: Response) => {
   try {
     const AuthHeader = req.headers.authorization;
@@ -14,30 +12,32 @@ const AddTasks = async (req: Request, res: Response) => {
     } else {
       try {
         // @ts-ignore
+        console.log(req.query);
+        res.status(401).json({ Message: "Please Login again!" });
 
-        const task = {
-          Title: req.body.Title,
-          isCompleted: false,
-        };
-        console.log(user.userId);
-        // Users.findById(user.userId).then((user) => {
-        //   if (user) {
-        //     user.Tasks.push(task);
-        //     user.save();
+        //   const task = {
+        //     Title: req.body.Title,
+        //     isCompleted: false,
+        //   };
+        //   console.log(user.userId);
+        //   // Users.findById(user.userId).then((user) => {
+        //   //   if (user) {
+        //   //     user.Tasks.push(task);
+        //   //     user.save();
+        //   //   }
+        //   // });
+
+        //   const userTasks = (await Users.findOneAndUpdate(
+        //     { _id: user.userId?.trim() },
+        //     { $push: { Tasks: task } }
+        //   )) as { isvalid: boolean; userId: string | null };
+
+        //   console.log(userTasks);
+        //   if (true) {
+        //     res.status(200).json({ Message: "Task Added Successfully!" });
+        //   } else {
+        //     res.status(500).json({ Message: "Invalid user!" });
         //   }
-        // });
-
-        const userTasks = (await Users.findOneAndUpdate(
-          { _id: user.userId?.trim() },
-          { $push: { Tasks: task } }
-        )) as { isvalid: boolean; userId: string | null };
-
-        console.log(userTasks);
-        if (true) {
-          res.status(200).json({ Message: "Task Added Successfully!" });
-        } else {
-          res.status(500).json({ Message: "Invalid user!" });
-        }
       } catch (err) {
         console.log(err);
         res
@@ -47,7 +47,9 @@ const AddTasks = async (req: Request, res: Response) => {
     }
   } catch (err) {
     console.log(err);
-    res.status(500).json({ Message: "Something went wrong on our side :(" });
+    res
+      .status(500)
+      .json({ Message: "Something went wrong on our side :(", error: err });
   }
 };
 
